f102bdbcd38bc45c96ccebe88fd189979547ab74
Ian Pickering <ipickering2@gmail.com>
2016-09-13 21:25:18 -0600

    windows

--- custom.el
+++ custom.el
@@ -3,19 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
+ '(package-selected-packages
    (quote
-    ("6df30cfb75df80e5808ac1557d5cc728746c8dbc9bc726de35b15180fa6e0ad9" default)))
- '(debug-on-error nil)
- '(haskell-interactive-popup-errors nil)
- '(haskell-mode-hook (quote (turn-on-haskell-indentation turn-on-haskell-doc)))
- '(haskell-process-auto-import-loaded-modules t)
- '(haskell-process-log t)
- '(haskell-process-suggest-remove-import-lines t)
- '(hipchat-autojoin (quote ("370715_starmonkey@conf.hipchat.com"))))
+    (yasnippet yari yaml-mode which-key web-mode spaceline solarized-theme smeargle shm shell-pop scss-mode ruby-block rspec-mode robe request rainbow-mode quickrun projectile-rails processing-mode powerline-evil popwin persp-mode org-bullets multi-term monokai-theme markdown-mode magit lua-mode lively kaomoji jabber helm-swoop helm-projectile helm-flycheck helm-flx helm-ag haml-mode google-translate google-this go-mode gitignore-mode git-timemachine ghc flycheck-haskell feature-mode expand-region evil-surround evil-smartparens evil-org evil-multiedit evil-matchit evil-leader evil-jumper evil-commentary enh-ruby-mode diminish dictionary crux company cider-eval-sexp-fu cider chruby bundler arduino-mode anzu ample-theme aggressive-indent))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(whitespace-trailing ((t (:foreground "tomato" :inverse-video t)))))
+ )
